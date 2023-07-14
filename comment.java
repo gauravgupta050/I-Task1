94aabfd50d540ed60f2ea0e61ea40a0e363af93c
Gaurav Gupta <92253785+gauravgupta050@users.noreply.github.com>
2023-07-14 13:39:06 +0530

    Add files via upload

---
+++ comment.java
@@ -0,0 +1,8 @@
+public class comment {
+    public static void main(String[] args) {
+        // The comment is getting executed....
+
+        System.out.println("Congrats , The comment gets executed!");
+    }
+    
+}
